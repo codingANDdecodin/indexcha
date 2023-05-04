a33e694a166021c5094d191fe27905b1ef69a293
codingANDdecodin <130179661+codingANDdecodin@users.noreply.github.com>
2023-05-04 13:22:23 +0530

    Update ContactMe.js

--- src/portfolioContainer/ContactMe/ContactMe.js
+++ src/portfolioContainer/ContactMe/ContactMe.js
@@ -48,7 +48,7 @@ export default function ContactMe(props) {
         message,
       };
       setBool(true);
-      const res = await axios.post(`/contact`, data);
+      const res = await axios.post(`https://myproject-ecma.onrender.com/contact`, data);
       if (name.length === 0 || email.length === 0 || message.length === 0) {
         setBanner(res.data.msg);
          toast.error(res.data.msg);
